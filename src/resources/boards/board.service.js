73cd44e0902be017b6b6a91220b2fd9bb4f46ef6
Sapranovich Andrey <SapranovichAndrey@yandex.ru>
2021-05-30 00:00:15 +0300

    feat: add descriptions services

--- src/resources/boards/board.service.js
+++ src/resources/boards/board.service.js
@@ -1,13 +1,33 @@
 const boardsRepo = require('./board.memory.repository');
-
+/**
+ * Get all boards.
+ * @returns {Promise<Array>} Array of Board objects.
+ */
 const getAllBoards = () => boardsRepo.getAllBoards();
-
+/**
+ * Get board by id.
+ * @param {string} boardId - boadr id. 
+ * @returns {Promise<Object>} Board object.
+ */
 const getBoard = (boardId) => boardsRepo.getBoard(boardId); 
-
+/**
+ * Create board
+ * @param {object} body - Boadr object. 
+ * @returns {Promise<Object>} Created Board object.
+ */
 const createBoard = (body) => boardsRepo.createBoard(body); 
-
+/**
+ * Remove board.
+ * @param {string} boardId - board id. 
+ * @returns {Promise<Object>} Removed Board object.
+ */
 const removeBoard = (boardId) => boardsRepo.removeBoard(boardId);
-
+/**
+ * Update board.
+ * @param {string} boardId - board id.
+ * @param {object} body - Board object.
+ * @returns {Promise<Object>} Updated Board object.
+ */
 const updateBoard = (boardId, body) => boardsRepo.updateBoard(boardId, body);
 
 module.exports = { getAllBoards, getBoard, createBoard, removeBoard, updateBoard };
--- src/resources/tasks/task.service.js
+++ src/resources/tasks/task.service.js
@@ -1,13 +1,37 @@
 const taskRepo = require('./task.memory.repository');
 
+/**
+ * Get all tasks specific board.
+ * @param {string} boardId - specific board id. 
+ * @returns {Promise<Array>} Array of Task objects.
+ */
 const getAllTasks = (boardId) => taskRepo.getAllTasks(boardId);
-
+/**
+ * Get task by id.
+ * @param {string} boardId - board id. 
+ * @param {string} taskId - task id.
+ * @returns {Promise<Object>} Task object.
+ */
 const getTask = (boardId, taskId) => taskRepo.getTask(boardId, taskId);
-
+/**
+ * Create task in specific board. 
+ * @param {string} boardId - board id. 
+ * @param {object} body - Task object
+ * @returns {Promise<Object>} created Task object.
+ */
 const createTask = (boardId, body) => taskRepo.createTask(boardId, body);
-
+/**
+ * Remove task by id.
+ * @param {string} taskId - task id. 
+ * @returns {Promise<Object>} removed Task object.
+ */
 const removeTask = (taskId) => taskRepo.removeTask(taskId);
-
+/**
+ * Update task by id.
+ * @param {string} taskId - task id.
+ * @param {object} body - Task object.
+ * @returns {Promise<Object>} updated Task object.
+ */
 const updateTask = (taskId, body) => taskRepo.updateTask(taskId, body);
 
 module.exports = {
